28201e394d864db4ac0386af1d482bf65e19376a
A-Janowski <janowskiadrian02@gmail.com>
2026-08-09 01:48:11 +0200

    add basic performance test

--- app/core/logger.py
+++ app/core/logger.py
@@ -8,7 +8,7 @@ def setup_logging():
 
     logger.add(
         sys.stdout, 
-        level="INFO",
+        level="TRACE",
         format="<level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>"
     )
 
